8a9929b923f572223194cc6ec0f68bb36f0175d9
Chris Withers <chris@simplistix.co.uk>
2016-06-15 09:18:06 +0100

    prepare for 0.10.0 release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ base_dir = os.path.dirname(__file__)
 setup(
     name='mortar_import',
     author='Chris Withers',
-    version='0.9',
+    version='0.10.0',
     author_email='chris@withers.org',
     license='MIT',
     description="Tools for importing data, particularly when using mortar_mixins",
