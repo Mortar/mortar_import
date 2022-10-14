25bfd3a5140e0b7486b4cad21b05a6f5909959b4
Chris Withers <chris@simplistix.co.uk>
2022-10-14 07:56:00 +0100

    WIP: Documentation

--- mortar_import/typing.py
+++ mortar_import/typing.py
@@ -1,9 +1,15 @@
 from typing import TypeVar, Hashable
 
+#: The hashable key extracted from existing and imported objects
+#: to decide if they represent the same object.
 Key = TypeVar('Key', bound=Hashable)
 
+#: An existing object.
 Existing = TypeVar('Existing')
+#: An imported object.
 Imported = TypeVar('Imported')
 
+#: An extracted existing object.
 ExtractedExisting = TypeVar('ExtractedExisting')
+#: An extracted imported object.
 ExtractedImported = TypeVar('ExtractedImported')
